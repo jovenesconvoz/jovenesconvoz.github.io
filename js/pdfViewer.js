bd8cf8840643cfbb8491a18c4fb785d28a4490a2
jovenesconvoz <166424755+jovenesconvoz@users.noreply.github.com>
2024-10-31 17:30:54 +0100

    Update pdfViewer.js

--- js/pdfViewer.js
+++ js/pdfViewer.js
@@ -1,7 +1,12 @@
-var pdfUrl = "assets/resources/manual.pdf";
+var pdfUrl1 = "assets/resources/manual.pdf";
+var pdfUrl2 = "assets/resources/bitacora.pdf";
 
 document.getElementById("viewbutton").addEventListener("click", function () {
-  window.open(pdfUrl, "_blank");
+  window.open(pdfUrl1, "_blank");
+});
+
+document.getElementById("viewbutton2").addEventListener("click", function () {
+  window.open(pdfUrl2, "_blank");
 });
 
 var statusMessage = document.getElementById("status-message");
@@ -13,7 +18,11 @@ function checkDownloadCapability() {
     statusMessage.textContent =
       "Il tuo browser non supporta il download diretto. Scegli l'opzione di visualizzazione.";
     document.getElementById("download-button").disabled = true;
+    document.getElementById("download-button").classList.add("disabled");
+    document.getElementById("download-button2").disabled = true;
+    document.getElementById("download-button2").classList.add("disabled");
   }
 }
 
 document.addEventListener("DOMContentLoaded", checkDownloadCapability);
+
